fcfde522e5128cfcf7c91d0101bb7fa2859db47e
ronghuiye <ronghui.ryan.ye@gmail.com>
2022-12-06 22:51:49 -0500

    add linting

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-import Task from './Task';
+import Task from './Task'
 
 export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
-  };
+  }
 
   const LoadingRow = (
     <div className="loading-item">
@@ -15,10 +15,10 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
         <span>Loading</span> <span>cool</span> <span>state</span>
       </span>
     </div>
-  );
+  )
   if (loading) {
     return (
-      <div className="list-items" data-testid="loading" key={"loading"}>
+      <div className="list-items" data-testid="loading" key={'loading'}>
         {LoadingRow}
         {LoadingRow}
         {LoadingRow}
@@ -26,29 +26,29 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
         {LoadingRow}
         {LoadingRow}
       </div>
-    );
+    )
   }
   if (tasks.length === 0) {
     return (
-      <div className="list-items" key={"empty"} data-testid="empty">
+      <div className="list-items" key={'empty'} data-testid="empty">
         <div className="wrapper-message">
           <span className="icon-check" />
           <p className="title-message">You have no tasks</p>
           <p className="subtitle-message">Sit back and relax</p>
         </div>
       </div>
-    );
+    )
   }
 
   const tasksInOrder = [
-    ...tasks.filter((t) => t.state === "TASK_PINNED"),
-    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
-  ];
+    ...tasks.filter((t) => t.state === 'TASK_PINNED'),
+    ...tasks.filter((t) => t.state !== 'TASK_PINNED'),
+  ]
   return (
     <div className="list-items">
       {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
-  );
+  )
 }
--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-import TaskList from './TaskList';
-import * as TaskStories from './Task.stories';
+import TaskList from './TaskList'
+import * as TaskStories from './Task.stories'
 
 export default {
   component: TaskList,
   title: 'TaskList',
-  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
-};
+  decorators: [(story) => <div style={{ padding: '3rem' }}>{story()}</div>],
+}
 
-const Template = args => <TaskList {...args} />;
+const Template = (args) => <TaskList {...args} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
   // Shaping the stories through args composition.
   // The data was inherited from the Default story in Task.stories.js.
@@ -23,9 +23,9 @@ Default.args = {
     { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
     { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
   ],
-};
+}
 
-export const WithPinnedTasks = Template.bind({});
+export const WithPinnedTasks = Template.bind({})
 WithPinnedTasks.args = {
   // Shaping the stories through args composition.
   // Inherited data coming from the Default story.
@@ -33,16 +33,16 @@ WithPinnedTasks.args = {
     ...Default.args.tasks.slice(0, 5),
     { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
   ],
-};
+}
 
-export const Loading = Template.bind({});
+export const Loading = Template.bind({})
 Loading.args = {
   tasks: [],
   loading: true,
-};
+}
 
-export const Empty = Template.bind({});
+export const Empty = Template.bind({})
 Empty.args = {
   ...Loading.args,
   loading: false,
-};
+}
